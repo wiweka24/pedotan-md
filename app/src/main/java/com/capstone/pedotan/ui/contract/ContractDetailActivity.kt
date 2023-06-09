56156189399ae8936daa342929dae33eb6e4868d
Ammar Raihan <ammarraihan00@gmail.com>
2023-06-09 16:12:44 +0700

    ui added

---
+++ app/src/main/java/com/capstone/pedotan/ui/contract/ContractDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.pedotan.ui.contract
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.pedotan.R
+
+class ContractDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contract_detail)
+    }
+}
---
+++ app/src/main/java/com/capstone/pedotan/ui/loan/LoanDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.pedotan.ui.loan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.pedotan.R
+
+class LoanDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loan_detail)
+    }
+}
